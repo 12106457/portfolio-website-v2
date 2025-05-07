c811ec8231fbf5d571549d3b5ac15bb4fe42e768
saifactiify <sai@factiify.com>
2025-05-07 16:58:43 +0530

    added Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      
+      <Analytics/>
       <Component {...pageProps} />
     </>
   );
